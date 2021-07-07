b1c02fba4d966b639c13f993db16ba7ed0bb3102
Abdulaxatov <samandarabdulaxatov55@gmail.com>
2021-07-07 23:27:39 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/* var elSiteHeader = document.querySelector(".site-header");
+var elSiteNavOpen = elSiteHeader.querySelector(".site-header__button");
+var elSiteNavClose = document.querySelector(".site-nav__btn")
+
+elSiteNavOpen.addEventListener("click", function (){
+  elSiteHeader.classList.add("site-nav--open");
+});
+
+elSiteNavClose.addEventListener("click", function(){
+  elSiteHeader.classList.remove("site-nav--open");
+}); */
